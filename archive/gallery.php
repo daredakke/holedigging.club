ce0bb9b568c42f0c39fb8c8cc31df767f350d1b6
daredakke <77235881+daredakke@users.noreply.github.com>
2023-04-01 14:27:42 +0100

    Reorganise gallery.php

--- archive/gallery.php
+++ archive/gallery.php
@@ -1,3 +1,61 @@
+<?php
+
+// $basePath = "https://holedigging.club/archive/";
+$basePath = "http://localhost/holedigging.club/archive/";
+$defaultImagesPerPage = 20;
+$validDirectories = array("artbook", "colours", "edits", "pics", "fanart", "misc", "screencaps");
+$directory = $_POST['directory'] ?? $_GET['directory'] ?? "pics";
+
+if (!in_array($directory, $validDirectories)) {
+  $directory = $validDirectories[0];
+}
+
+$pdo = new PDO("sqlite:archive.db") or die("cannot open the database");
+
+// Get number of images in specified directory
+$stmt = $pdo->prepare("SELECT COUNT(*) AS totalImages FROM archive WHERE image_path LIKE '%images/' || ? || '/%'");
+$stmt->execute([$directory]);
+$result = $stmt->fetch();
+
+$totalImages = $result ? $result[0] : 0;
+
+$imagesPerPage = $_POST["imagesPerPage"] ?? $_GET["imagesPerPage"] ?? $defaultImagesPerPage;
+
+if (is_numeric($imagesPerPage)) {
+  $imagesPerPage = floor($imagesPerPage);
+
+  if ($imagesPerPage < 1) {
+    $imagesPerPage = 1;
+  }
+  if ($imagesPerPage > $totalImages) {
+    $imagesPerPage = $totalImages;
+  }
+} else {
+  $imagesPerPage = $defaultImagesPerPage;
+}
+
+$maxPage = 1;
+
+if ($totalImages > 0) {
+  $maxPage = ceil($totalImages / $imagesPerPage);
+}
+
+$page = $_POST['page'] ?? $_GET['page'] ?? 1;
+
+if (is_numeric($page)) {
+  $page = floor($page);
+
+  // Keep us between page 1 and page max
+  $page = $page < 1 ? 1 : $page;
+  $page = $page > $maxPage ? $maxPage : $page;
+} else {
+  $page = 1;
+}
+
+$offset = ($page - 1) * $imagesPerPage;
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -12,115 +70,66 @@
 <body>
   <div class="gallery">
     <?php
-      // $basePath = "https://holedigging.club/archive/";
-      $basePath = "http://localhost/holedigging.club/archive/";
-      $defaultImagesPerPage = 15;
-      $validDirectories = array("artbook", "colours", "edits", "pics", "fanart", "misc", "screencaps");
-      $directory = $_POST['directory'] ?? $_GET['directory'] ?? "pics";
 
-      if (!in_array($directory, $validDirectories)) {
-        $directory = $validDirectories[0];
+    // Output images
+    $sqlGetImages = "SELECT image_path, thumbnail_path FROM archive WHERE image_path LIKE '%images/' || ? || '/%' ORDER BY image_path LIMIT ? OFFSET ?";
+    $stmt = $pdo->prepare($sqlGetImages);
+    $stmt->execute([$directory, $imagesPerPage, $offset]);
+    $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    if ($rows) {
+      foreach ($rows as $row) {
+        $imagePath = $row["image_path"];
+        $thumbnailPath = $row["thumbnail_path"];
+        echo "<a class=\"image-link\" href=\"$imagePath\" target=\"_blank\"> <img src=\"$thumbnailPath\"> </a>";
       }
+    } else {
+      echo "0 images found :(";
+    }
 
-      $pdo = new PDO("sqlite:archive.db") or die("cannot open the database");
+    $pdo = null;
 
-      // Get number of images in specified directory
-      $stmt = $pdo->prepare("SELECT COUNT(*) AS totalImages FROM archive WHERE image_path LIKE '%images/' || ? || '/%'");
-      $stmt->execute([$directory]);
-      $result = $stmt->fetch();
-
-      $totalImages = $result ? $result[0] : 0;
-
-      $imagesPerPage = $_POST["imagesPerPage"] ?? $_GET["imagesPerPage"] ?? $defaultImagesPerPage;
-
-      if (is_numeric($imagesPerPage)) {
-        $imagesPerPage = floor($imagesPerPage);
-
-        if ($imagesPerPage < 1) {
-          $imagesPerPage = 1;
-        }
-        if ($imagesPerPage > $totalImages) {
-          $imagesPerPage = $totalImages;
-        }
-      } else {
-        $imagesPerPage = $defaultImagesPerPage;
-      }
-
-      $maxPage = 1;
-
-      if ($totalImages > 0) {
-        $maxPage = ceil($totalImages / $imagesPerPage);
-      }
-
-      $page = $_POST['page'] ?? $_GET['page'] ?? 1;
-
-      if (is_numeric($page)) {
-        $page = floor($page);
-
-        // Keep us between page 1 and page max
-        $page = $page < 1 ? 1 : $page;
-        $page = $page > $maxPage ? $maxPage : $page;
-      } else {
-        $page = 1;
-      }
-
-      $offset = ($page - 1) * $imagesPerPage;
-
-      // Output images
-      $sqlGetImages = "SELECT image_path, thumbnail_path FROM archive WHERE image_path LIKE '%images/' || ? || '/%' ORDER BY image_path LIMIT ? OFFSET ?";
-      $stmt = $pdo->prepare($sqlGetImages);
-      $stmt->execute([$directory, $imagesPerPage, $offset]);
-      $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-      if ($rows) {
-        foreach ($rows as $row) {
-          $imagePath = $row["image_path"];
-          $thumbnailPath = $row["thumbnail_path"];
-          echo "<a class=\"image-link\" href=\"$imagePath\" target=\"_blank\"> <img src=\"$thumbnailPath\"> </a>";
-        }
-      } else {
-        echo "0 images found :(";
-      }
-
-      $pdo = null;
     ?>
   </div>
 
   <div class="gallery-controls">
     <div class="gallery-pagination">
       <?php
-        if ($maxPage > 1) {
-          $url = "${basePath}gallery.php?directory=$directory&imagesPerPage=$imagesPerPage&page=";
-          
-          // Determine start and end for numbered page links
-          $start = $page - 2 < 1 ? 1 : $page - 2;
-          $end = $page + 2 > $maxPage ? $maxPage : $page + 2;
 
-          // First and previous page
-          if ($page != 1) {
-            echo "<a href='${url}1'>&lt;&lt;</a>";
-            $prev = $page - 1;
-            echo "<a href='$url$prev'>&lt;</a>";
-          }
+      // Pagination
+      if ($maxPage > 1) {
+        $url = "${basePath}gallery.php?directory=$directory&imagesPerPage=$imagesPerPage&page=";
+        
+        // Determine start and end for numbered page links
+        $start = $page - 2 < 1 ? 1 : $page - 2;
+        $end = $page + 2 > $maxPage ? $maxPage : $page + 2;
 
-          // Current and surrounding pages
-          for ($i = $start; $i <= $end; $i++) {
-            $pageNum = strlen((string) $i) < 2 ? "0$i" : $i;
+        // First and previous page
+        if ($page != 1) {
+          echo "<a href='${url}1'>&lt;&lt;</a>";
+          $prev = $page - 1;
+          echo "<a href='$url$prev'>&lt;</a>";
+        }
 
-            if ($i == $page) {
-              echo "<p>$pageNum</p>";
-            } else {
-              echo "<a href='$url$i'>$pageNum</a>";
-            }
-          }
+        // Current and surrounding pages
+        for ($i = $start; $i <= $end; $i++) {
+          $pageNum = strlen((string) $i) < 2 ? "0$i" : $i;
 
-          // Next and last page
-          if ($page < $maxPage) {
-            $next = $page + 1;
-            echo "<a href='$url$next'>&gt;</a>";
-            echo "<a href='$url$maxPage'>&gt;&gt;</a>";
+          if ($i == $page) {
+            echo "<p>$pageNum</p>";
+          } else {
+            echo "<a href='$url$i'>$pageNum</a>";
           }
         }
+
+        // Next and last page
+        if ($page < $maxPage) {
+          $next = $page + 1;
+          echo "<a href='$url$next'>&gt;</a>";
+          echo "<a href='$url$maxPage'>&gt;&gt;</a>";
+        }
+      }
+
       ?>
     </div>
 
@@ -129,13 +138,15 @@
         <label for="directory">Gallery</label>
         <select name="directory">
           <?php
-            foreach ($validDirectories as $dir) {
-              if ($dir == $directory) {
-                echo "<option selected value=\"$dir\">$dir</option>";
-              } else {
-                echo "<option value=\"$dir\">$dir</option>";
-              }
+
+          foreach ($validDirectories as $dir) {
+            if ($dir == $directory) {
+              echo "<option selected value=\"$dir\">$dir</option>";
+            } else {
+              echo "<option value=\"$dir\">$dir</option>";
             }
+          }
+
           ?>
         </select>
       </div>
